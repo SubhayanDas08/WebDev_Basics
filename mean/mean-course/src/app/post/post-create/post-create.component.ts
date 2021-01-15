fe1ae8c0b54a6dc719dbfdb4d3392b656ee941a1
SubhayanDas08 <nips.subhayan@gmail.com>
2021-01-15 14:32:07 +0530

    15th Jan 2021

--- mean/mean-course/src/app/post/post-create/post-create.component.ts
+++ mean/mean-course/src/app/post/post-create/post-create.component.ts
@@ -5,4 +5,8 @@ import { Component } from '@angular/core';
   templateUrl: './post-create.component.html'
 })
 
-export class PostCreateComponent{}
+export class PostCreateComponent{
+  onAddPost(){
+    alert('Post Added!!!');
+  }
+}

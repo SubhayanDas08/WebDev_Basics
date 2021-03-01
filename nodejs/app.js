523752fdbd01e080c6025d619c9d6506139f122c
SubhayanDas08 <nips.subhayan@gmail.com>
2021-03-01 18:16:41 +0530

    1st Mar  2021

--- nodejs/app.js
+++ nodejs/app.js
@@ -2,4 +2,6 @@ const tutorial = require('./tutorials');
 
 console.log(tutorial);
 
-console.log(tutorial(4, 1));
+console.log(tutorial.sum(4, 1));
+console.log(tutorial.PI);
+console.log(new tutorial.SomeMathObject());
--- nodejs/tutorials.js
+++ nodejs/tutorials.js
@@ -1,3 +1,19 @@
 const sum = (num1, num2) => num1 + num2;
 
-module.exports = sum;
+const PI = 3.14;
+
+class SomeMathObject {
+    constructor() {
+        console.log('Object Created!');
+    }
+}
+
+// module.exports.sum = sum;
+// module.exports.PI = PI;
+// module.exports.SomeMathObject = SomeMathObject;
+
+module.exports = {
+    sum: sum,
+    PI: PI,
+    SomeMathObject: SomeMathObject
+};

fafe7f92a2c00d3326cf83157c55b01f8a916cb1
SubhayanDas08 <nips.subhayan@gmail.com>
2021-03-06 11:37:45 +0530

    6th Mar  2021

--- nodejs/app.js
+++ nodejs/app.js
@@ -82,53 +82,96 @@
 //     console.log('Correct!!');
 // });
 
+// --------------------------
+
 //WORKING WITH FILE SYSTEM MODULE
+
+// const fs = require('fs');
+
+// //create a file
+
+// fs.writeFile('example.txt', 'This is an Example', (err) => {
+//     if (err) {
+//         console.log(err);
+//     } else {
+//         console.log('Successfully Created!!');
+//     }
+// });
+
+// //read a file
+
+// fs.readFile('example.txt', 'utf8', (err, File) => {
+//     if (err) {
+//         console.log(err);
+//     } else {
+//         console.log(File);
+//     }
+// });
+
+// // renaming a file
+
+// fs.rename('example.txt', 'example2.txt', (err) => {
+//     if (err)
+//         console.log(err);
+//     else
+//         console.log('Successfully Renamed the File');
+// });
+
+// //append data to a file
+
+// fs.appendFile('example2.txt', ' Adding data to the file', (err) => {
+//     if (err)
+//         console.log(err);
+//     else
+//         console.log('Successfully Appended data');
+// });
+
+// //deleting a file
+
+// fs.unlink('example2.txt', (err) => {
+//     if (err)
+//         console.log(err);
+//     else
+//         console.log('Successfully Deleted the file');
+// });
+
+//
+
+// --------------------------
+
+//WORKING WITH THE FILE SYSTEM MODULE PT2
 
 const fs = require('fs');
 
-//create a file
+//creating a folder
 
-fs.writeFile('example.txt', 'This is an Example', (err) => {
-    if (err) {
+fs.mkdir('tutorial', (err) => {
+    if (err)
         console.log(err);
-    } else {
-        console.log('Successfully Created!!');
+    else
+        console.log('Successfully Created a folder');
+});
+
+//deleting a folder
+
+fs.rmdir('tutorial', (err) => {
+    if (err)
+        console.log(err);
+    else
+        console.log('Successfully Deleted a folder');
+});
+
+//creating a file inside a folder
+
+fs.mkdir('example', (err) => {
+    if (err)
+        console.log(err);
+    else {
+        fs.writeFile('./example/a.txt', '1234567890', (err) => {
+            if (err)
+                console.log(err);
+            else
+                console.log('Successfully Added File to a example folder');
+        });
     }
-});
-
-//read a file
-
-fs.readFile('example.txt', 'utf8', (err, File) => {
-    if (err) {
-        console.log(err);
-    } else {
-        console.log(File);
-    }
-});
-
-// renaming a file
-
-fs.rename('example.txt', 'example2.txt', (err) => {
-    if (err)
-        console.log(err);
-    else
-        console.log('Successfully Renamed the File');
-});
-
-//append data to a file
-
-fs.appendFile('example2.txt', ' Adding data to the file', (err) => {
-    if (err)
-        console.log(err);
-    else
-        console.log('Successfully Appended data');
-});
-
-//deleting a file
-
-fs.unlink('example2.txt', (err) => {
-    if (err)
-        console.log(err);
-    else
-        console.log('Successfully Deleted the file');
 });
